db9a47f3964a06f6312e62d020d368e3926d7de4
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-02-19 22:57:16 +0100

    First commit - user can get weather information about a specific locatoin

---
+++ src/component/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from './Header/';
+export { default as Content } from './Content/Content';
+export { default as Form } from './Form/Form';
+export { default as Footer } from './Footer/Footer';
