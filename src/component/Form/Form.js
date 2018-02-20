2d5a6f1313c590d2e149556418146bb6cdb15c6e
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-02-20 14:44:24 +0100

    Add geolocation, also changed api type - gets weather rapport for 10 days.

--- src/component/Form/Form.js
+++ src/component/Form/Form.js
@@ -11,59 +11,49 @@ class Form extends Component {
     onSubmit(e) {
       e.preventDefault();
   
-      const cityname = e.nativeEvent.target.elements[0].value;
-      const apiKey = `77c7d99a124f26c235b411cb8645d14b`;
-      
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&APPID=${apiKey}&units=metric&cnt=7`)
-    // fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?q=${cityname}&APPID=${apiKey}&cnt=10`)
+        const cityname = e.nativeEvent.target.elements[0].value;
+        const apiKey = `77c7d99a124f26c235b411cb8645d14b`;
+        const apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityname}&APPID=${apiKey}&units=metric&cnt=10`;
+        
+        
+    fetch(apiURL)
         .then(res => res.json())
         .then(res => {
           this.setState({
-            weather: res.weather,
-            name: res.name,
-            main: res.main.temp,
-            sunrise: res.sys.sunrise,
-            sunset: res.sys.sunset
+            city: res.city.name,
+            list: res.list.main,
+            temp: res.list[0].main.temp,
+            dt_txt: res.list[0].dt_txt
             
         }, function() {
-            console.log(res);
-            console.log(res.sys.sunset);
-            // console.log(res.forecast.precipitation.unit); Not working yet.
-            console.log('Hopefully we have some weather', 
-                this.state.weather, 
-                this.state.name, 
-                this.state.main, 
-                this.state.sunrise, 
-                this.state.sunset);
+
+            //Gets coordination for current location
+            if("geolocation" in navigator) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    console.log(position);
+                });
+            }
+
           })
         });
-    }
-  
-    render() {
-      return (
-        <div>
-          <form onSubmit={this.onSubmit.bind(this)}>
-            <input type="text" placeholder="Type the city name here" name="city" />
-            <button type="submit">Get weather</button>
-          </form>
-          { this.state.weather.length > 0 ? 
-            <div className="App-weather">
-              <h3>
-                  The current weather in {this.state.name}:    
-                  </h3>
-              <img src={`http://openweathermap.org/img/w/${this.state.weather[0].icon}.png`} title="Title goes here" alt="A weather icon, describing the... weather" />
-               <p>{this.state.weather[0].description} </p>
-               <p>
-                    {this.state.main} degrees Celsius
-               </p>
 
-              
-            </div>
-            : <p>No results yet</p>
-          }
-        </div>
-      );
+
     }
-  }
+    render() {
+        return (
+          <div>
+            <form onSubmit={this.onSubmit.bind(this)}>
+              <input type="text" placeholder="Type the city name here" name="city" />
+              <button type="submit">Get weather</button>
+            </form>
+            <p> { this.state.dt_txt } </p>
+            <p> { this.state.city } </p>
+            <p> { this.state.temp }°C </p>
+          </div>
+        );
+      }
+    }
+
   
   export default Form;
+
