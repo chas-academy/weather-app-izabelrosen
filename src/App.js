c1aa8fcfa3e96bb1fd2ebae13e5771e1957e35e0
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-02-23 16:18:07 +0100

    Add react routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-// import { Header } from './component/Header';
-// import { Footer } from './component/Footer/Footer';
+
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import { Header, Content, Form, Location, Today, Footer} from './component';
 
 import './App.css';
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Content> 
-          <Today />
-            <Form />
-            <Location />
-        </Content>
+        {/* <div>
+          <ul className="header">
+            <li><Link to ="/">Home</Link></li>
+            <li><Link to ="/search">Search</Link></li> 
+            <li><Link to ="/today">Today's Weather</Link></li>
+            <li><Link to ="/current">5 days forecast</Link></li> 
+          </ul> */}
+
+           <Content> 
+            <Route path="/search" component={Form} />
+            <Route path="/today" component={Today} />
+            <Route path="/current" component={Location} />
+            </Content>
+          {/* </div> */}
         <Footer />
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router} from "react-router-dom";
+import App from "./App";
+
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
