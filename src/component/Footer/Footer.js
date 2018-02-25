9bae61fde44035cf714983125a0f20a2e7fbb355
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-02-25 23:10:12 +0100

    Minor footer styling

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,11 +1,12 @@
 import React, { Component} from 'react';
+import './Footer.css';
 
 
 class Footer extends Component {
     render() {
         return (
             <footer className="App-footer">
-                <p>Footer info here</p>
+                <p>The weather is sweet</p>
             </footer>
         )
     }
