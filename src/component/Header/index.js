5cc9a471b8bb5f39871f631b90e12f68a2cb1d0d
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-02-25 21:41:43 +0100

    Add header navbar and styling

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -1,17 +1,43 @@
+// import React, { Component} from 'react';
+// import logo from './logo.svg';
+
+// import './Header.css';
+
+//  class Header extends Component {
+//      render() {
+//          return (
+//              <header className="App-header">
+//                  <img src={logo} className="App-logo" alt="logo" />
+//                  <h1 className="App-title">Weather</h1>
+//              </header>
+//          )
+//      };
+//     }
+ 
+//  export default Header;
+
 import React, { Component} from 'react';
-import logo from './logo.svg';
 
-import './Header.css';
+import { Link } from "react-router-dom";
+// import { Form } from './component';
 
-class Header extends Component {
-    render() {
-        return (
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Weather</h1>
-            </header>
-        )
-    }
-}
 
+
+
+const Header = () => (
+  <header>
+      {/* <h1> Weather </h1> */}
+    <nav>
+    <div>
+          <ul className="header">
+            <li><Link to ="/">Home</Link></li>
+            <li><Link to ="/search">Search</Link></li> 
+            {/* <li><Link to ="/search"><Form /></Link></li>  */}
+            <li><Link to ="/today">Today's Weather</Link></li>
+            <li><Link to ="/current">5 days forecast</Link></li> 
+          </ul>
+          </div>
+    </nav>
+  </header>
+);
 export default Header;
