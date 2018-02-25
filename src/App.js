cc7aa2a92b13f09b1291aa7f1e831f355a2acbf5
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-02-25 23:12:15 +0100

    Delete unused code, add styling

--- src/App.js
+++ src/App.js
@@ -11,20 +11,12 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        {/* <div>
-          <ul className="header">
-            <li><Link to ="/">Home</Link></li>
-            <li><Link to ="/search">Search</Link></li> 
-            <li><Link to ="/today">Today's Weather</Link></li>
-            <li><Link to ="/current">5 days forecast</Link></li> 
-          </ul> */}
-
            <Content> 
             <Route path="/search" component={Form} />
             <Route path="/today" component={Today} />
-            <Route path="/current" component={Location} />
+            <Route path="/5-days-forecast" component={Location} />
             </Content>
-          {/* </div> */}
+            
         <Footer />
       </div>
     );
--- src/component/Today/Today.js
+++ src/component/Today/Today.js
@@ -39,66 +39,35 @@ class Today extends Component {
                     wind: res.wind.speed,
                     
                   }, function() {
-                       console.log(res.wind.speed);
+                    //    console.log(res.wind.speed);
           
                     })
                   });
             });
 
     }
-    renderToday() {
-        var moment = require('moment');
-    
-        if (this.state.weather.length) {
-    
-            // console.log(this.state.weather[0]);
-            
-             return (
-                 this.state.weather
-                 .map((day, i) => 
-                 <ul key={i} id="weather-day"> 
-                     <li>{moment(day.dt_txt).format("D/MM/YYYY")}</li>
-                     <li>{moment(day.dt_txt).format("HH.mm")}</li>
-                     <li><img src={`http://openweathermap.org/img/w/${day.weather[0].icon}.png`} /></li>
-                     <li>Sunrise: {day.sys.sunrise}</li>
-                     <li>Sunset: {day.sys.sunset}</li>
-              
-                 </ul>
-             )
-    
-             )
-        } else {
-            // return <p>Loading …</p>
-        }
-        
-    }
 
     render() {
 
-        var moment = require('moment');
-        var sunrise = moment.unix(this.state.sunrise).utc(Date);
-        var sunset = moment.unix(this.state.sunset).utc(Date);
+        const moment = require('moment');
+        const sunrise = moment.unix(this.state.sunrise).utc(Date);
+        const sunset = moment.unix(this.state.sunset).utc(Date);
 
         const celsius = (this.state.temp);
         const fahrenheit = celsius * 9 / 5 + 32;
         const bothTemp = `${celsius}°C is ${fahrenheit}°F`;
-          console.log(bothTemp);
-
-        
         
         return (
             <div>
-                <h2>{moment(this.state.dt_txt).format("dddd D/MM")}</h2>
+                <h2>📍</h2>
                 <h3>{ this.state.name }</h3>
-                <h3> Temperature: <br /> { this.state.temp }°C </h3>
-                <h3> {fahrenheit}°F </h3>
+                <h4> Temperature: <br /> {Math.floor(this.state.temp) }°C </h4>
+                <h4> {Math.floor(fahrenheit)}°F </h4>
                 <p> Wind: { this.state.wind } m/s </p>
-                <p> Humidity: { this.state.wind } % </p>
+                <p> Humidity: { this.state.humidity } % </p>
                 <p> Sunrise: <br />{moment(sunrise).format("HH.mm")} </p>
                 <p> Sunset: <br />{moment(sunset).format("HH.mm")} </p>
                 
-
-                 { this.renderToday() }
                   
             </div>
         );
